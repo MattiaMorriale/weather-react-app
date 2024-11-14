33e71990a279053a3353c28b577fb776221bfcd7
mattia_morriale <mattia.morriale1@gmail.com>
2024-11-14 18:48:36 +0100

    search function completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
+/*  Milestone 1: Struttura base e funzionalità core
+Creazione del progetto React: Inizializza un nuovo progetto React utilizzando Create React App.
+Componente principale: Crea un componente principale che renderizzerà l'intera applicazione.
+Input per la città: Aggiungi un input text per permettere all'utente di inserire il nome della città.
+Bottone di ricerca: Implementa un pulsante per inviare la richiesta all'API quando l'utente clicca su di esso.
+Stato iniziale: Utilizza useState per gestire lo stato iniziale dell'applicazione, come il nome della città inserita e i dati meteo ricevuti.
+Richiesta all'API: Utilizza fetch per inviare una richiesta all'API OpenWeatherMap e ottenere i dati meteo.
+Visualizzazione dei dati: Visualizza le informazioni meteo di base (temperatura, umidità, ecc.) in un formato leggibile.
+
+Milestone 2: Miglioramento dell'interfaccia utente
+Stile: Applica uno stile base all'applicazione utilizzando CSS o una libreria di styling come styled-components o Material-UI.
+Layout: Organizza i componenti in un layout chiaro e intuitivo.
+Icone: Utilizza una libreria di icone (es. React Icons) per visualizzare icone rappresentative delle condizioni meteo.
+Caricamento: Mostra un indicatore di caricamento mentre i dati meteo vengono recuperati.
+Messaggi di errore: Gestisci gli errori che potrebbero verificarsi durante la richiesta all'API e mostra all'utente un messaggio appropriato.
+
+
+Milestone 3: Funzionalità avanzate
+Unità di misura: Permetti all'utente di scegliere tra gradi Celsius e Fahrenheit.
+Previsioni multiple: Recupera le previsioni meteo per i prossimi giorni e visualizzale in un grafico o in una tabella.
+Localizzazione automatica: Utilizza la geolocalizzazione del dispositivo per determinare la posizione dell'utente e visualizzare le previsioni per quella posizione.
+Salvataggio dei preferiti: Permetti all'utente di salvare le città preferite e visualizzarne rapidamente le previsioni.
+Notifiche: Implementa un sistema di notifiche per avvisare l'utente di condizioni meteo avverse (es. tempeste, temperature estreme).
+
+Bonus
+Dark mode: Aggiungi un'opzione per passare alla modalità scura.
+Widget personalizzabile: Permetti all'utente di personalizzare il widget meteo scegliendo quali informazioni visualizzare.
+Integrazione con un calendario: Mostra le previsioni meteo in un calendario.
+Animazioni: Aggiungi delle animazioni per rendere l'interfaccia utente più dinamica.
+Test unitari: Scrivi dei test unitari per assicurarti che il tuo codice funzioni correttamente.
+ Deploy: Deploya l'applicazione su una piattaforma di hosting come Netlify o Vercel. */
+
+
 import { useState } from 'react'
 import './App.css'
+import MainComponent from './components/MainComponent'
 
 function App() {
 
   return (
     <>
-      
+      <MainComponent></MainComponent>
     </>
   )
 }
---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './MainComponent.css'
+
+function MainComponent() {
+
+    const [input, setInput] = useState("");
+    const [selectedCity, setSelectedCity] = useState([]);
+    const [weatherData, setWeatherData] = useState({});
+
+    const apiUrl = 'https://geocoding-api.open-meteo.com/v1/search?name=';
+    const apiUrlWeather = 'https://api.open-meteo.com/v1/forecast?';
+
+    let arrayListItems = [];
+
+    const handleChange = (e) => {
+      setInput(e.target.value);
+      if (input.length >= 3) {
+        axios.get(`${apiUrl}${e.target.value}`).then((dataCities) => {
+          
+          let cities = dataCities.data.results;
+
+          cities.map((name, index) =>
+            arrayListItems.push(
+              <li key={name.name + index} onClick={searchData} className='px-2.5 hover:bg-slate-200 cursor-pointer'>
+                {name.name}, {name.admin1}, {name.country}
+              </li>
+            ),
+          )
+
+          setSelectedCity(arrayListItems)
+        })
+      }
+    };
+
+    const searchData = (e) => {
+      let latitudine;
+      let longitudine;  
+
+      e.preventDefault();
+      
+      axios.get(`${apiUrl}${input}`).then((city) => {
+        latitudine = city.data.results[0].latitude;
+        longitudine = city.data.results[0].longitude;
+
+        retrieveData(latitudine, longitudine);
+      })
+      console.log(input);
+
+      setInput("");
+    };
+
+    const retrieveData = (latitudine, longitudine) => {
+      axios.get(`${apiUrlWeather}latitude=${latitudine}&longitude=${longitudine}&hourly=temperature_2m`).then((temperature) => {
+        console.log(temperature)
+      })
+    };
+
+    
+
+  return (
+    <>
+      <div className='p-5 flex justify-between items-center bg-slate-400'>
+        <div>
+          <h1 className='text-3xl uppercase text-white font-bold'>React Weather</h1>
+        </div>
+        <div className='relative max-w-sm w-full'>
+          <form onSubmit={searchData}>
+            <div className={input.length >= 3 ? 'bg-white px-2.5 py-2 rounded-t-3xl flex items-center' : 'bg-white px-2.5 py-2 rounded-3xl flex items-center'} >
+              <input type="text" className='py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent appearance-none focus:outline-none focus:ring-0 peer' value={input} onChange={handleChange} placeholder='Search a location'/>
+              <button className=' px-3 py-1 rounded-3xl bg-slate-400 text-white font-bold' type='submit'>
+                <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                </svg>
+              </button>
+            </div>
+          </form>
+          {input.length >= 3 && 
+          <div className='bg-white border absolute py-4 rounded-b-3xl w-full mt-3'>
+            <ul>
+              {selectedCity}
+            </ul>
+          </div>
+          }
+        </div>
+      </div>
+
+{/* ti sei fermato qua... devi stampare le robe... fallo o ti uccido nel sonno */}
+
+      <div className='p-5 grid-rows-1'>
+        <div href="#" class="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{input}</h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+      </div>
+      <div className='p-5 flex gap-5'>
+        <div href="#" class="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+        <div href="#" class="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MainComponent
